0732eab23d34d52861060c6dab4c1b8452a81470
Gary A. Newsome <garyanewsome@gmail.com>
2018-12-02 09:58:53 -0500

    angular 1.7.5 template

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+angular.module('app', [])
+  .controller('myCtrl', function($scope) {
+    angular.extend(this, {})
+    $scope.initialize = function() {}
+    angular.element(document).ready(function () {})
+  })
